374d2acfee844184dc27c2e4f2c07f036a20799a
hasitha <hasithadkr7@gmail.com>
2020-08-08 00:59:21 +0530

    set mail trigger

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ if __name__ == '__main__':
     config_path = '/home/hasitha/PycharmProjects/EmailNotifier/config.json'
     parameter_type = 'water_level'
     models = ['flo2d_250']
+    lead_time_hours = 12
     try:
-        notify(config_path, parameter_type, models)
+        notify(config_path, parameter_type, models, lead_time_hours)
     except Exception as e:
         print('Exception in main:', str(e))
         traceback.print_exc()
