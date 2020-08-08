ecd491f25006bcebe8be6b1ded86e2969c526a5a
hasitha <hasithadkr7@gmail.com>
2020-08-08 09:44:17 +0530

    config path update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from notifier import notify
 
 
 if __name__ == '__main__':
-    config_path = '/home/hasitha/PycharmProjects/EmailNotifier/config.json'
+    config_path = '/home/uwcc-admin/EmailNotifier/config.json'
     try:
         notify(config_path, 'water_level', ['flo2d_250', 'flo2d_150', 'flo2d_150_v2'], 6)
     except Exception as e:
