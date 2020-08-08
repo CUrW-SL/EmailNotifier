7ff4c47ea84032ecc3d15fff6677f0b26c176983
hasitha <hasithadkr7@gmail.com>
2020-08-08 16:46:29 +0530

    mail send code refactoring

--- send_mail.py
+++ send_mail.py
@@ -11,21 +11,6 @@ def send_email(email_config, email_subject, email_content, recipients_list):
         print("Successfully connected to the SMTP server at %s:%s !!"
               % (email_config['host'], email_config['port']))
 
-        # create a multi-part email message
-        # email_message = MIMEMultipart()
-        # # setup the parameters of the message
-        # email_message['From'] = email_config['user-name']
-        # email_message['To'] = ", ".join(recipients_list)
-        # email_message['Subject'] = email_subject
-        # print(email_message['Subject'])
-        # # add in the message body
-        # email_message.attach(MIMEText(email_content, 'plain'))
-        # send the message via the server set up earlier.
-        # smtp_server.send_message(email_message)
-        # message = """Subject: {}
-        #
-        # {}""".format(email_subject, email_content)
-
         message = 'Subject: {}\n\n{}'.format(email_subject, email_content)
 
         smtp_server.sendmail(email_config['user-name'], recipients_list, message)
