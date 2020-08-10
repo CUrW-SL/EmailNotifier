935add7f6635140d36d234f63e82cd87d00db496
hasitha <hasithadkr7@gmail.com>
2020-08-10 14:13:48 +0530

    remove 150 v2 from alert list

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from notifier import notify
 if __name__ == '__main__':
     config_path = '/home/uwcc-admin/EmailNotifier/config.json'
     try:
-        notify(config_path, 'water_level', ['flo2d_250', 'flo2d_150', 'flo2d_150_v2'], 6)
+        notify(config_path, 'water_level', ['flo2d_250', 'flo2d_150'], 6)
     except Exception as e:
         print('Exception in main:', str(e))
         traceback.print_exc()
